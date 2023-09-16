c8c239ffed675bae03e03d43312ed70e018cf1d3
junya.soccer.3jsb <junya.soccer.3jsb@gmail.com>
2023-09-16 22:22:19 +0900

    add vuetify plugin

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify({
+    // Vuetifyのカスタム設定を追加することができます
+});
+
+export default vuetify;
